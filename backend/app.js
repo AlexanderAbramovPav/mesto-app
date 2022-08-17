4b2757515da12bcf4df1028cab314341f2886b84
Alexander Abramov <abramov.alex.p@yandex.ru>
2022-08-18 00:38:58 +0300

    fix CORS end

--- backend/app.js
+++ backend/app.js
@@ -44,7 +44,7 @@ app.use((req, res, next) => {
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.end();
+    // res.end();
     return;
   }
 
