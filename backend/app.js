21beb85b8db028fbe933b1927d0d52c588ffb0b7
Alexander Abramov <abramov.alex.p@yandex.ru>
2022-08-18 22:21:40 +0300

    fix

--- backend/app.js
+++ backend/app.js
@@ -50,6 +50,7 @@ app.use((req, res, next) => {
   }
 
   if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Origin', 'https://alexander.abramov.nomoredomains.sbs');
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     res.header('Access-Control-Allow-Credentials', true);
