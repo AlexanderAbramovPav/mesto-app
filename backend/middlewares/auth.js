179e6a700e30d7b5278437d9079e9dd8d3a02bf2
Alexander Abramov <abramov.alex.p@yandex.ru>
2022-08-19 14:27:52 +0300

    awdwa

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,7 +3,7 @@ const UnauthorizedError = require('../errors/unauthorized-err');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.cookies.jwt;
-  console.log(req.cookies);
+  console.log(req.cookies.jwt);
   if (!authorization || !authorization.startsWith('Bearer ')) {
     throw new UnauthorizedError('Необходима авторизация');
   }
