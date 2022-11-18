ee80da433a5fc88c53f2b089ca06363667cdd1db
Alexander Abramov <abramov.alex.pav@gmail.com>
2022-11-18 21:12:06 +0300

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -104,7 +104,7 @@ export default class Api {
 }
 
 export const apiSettings = new Api({
-  baseUrl: "https://api.alexander.abramov.nomoredomains.sbs",
+  baseUrl: "https://react-mesto-api-full-1lop.vercel.app",
   headers: {
     "Content-Type": "application/json",
   },
