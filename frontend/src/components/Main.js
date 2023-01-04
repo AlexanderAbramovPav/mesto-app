560ee637f8706c6ce5ef6bcfcbc246c85ca89b94
Alexander Abramov <abramov.alex.p@yandex.ru>
2023-01-05 00:41:09 +0300

    add sign-bg and infinite scroll

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,13 +1,44 @@
 import React from 'react'
 import Card from './Card';
 import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
-import { useQuery } from 'react-query' 
-import { fetchUserInfo, fetchUserCards } from "../utils/apiQuery"; 
+import { useQuery, useInfiniteQuery } from 'react-query' 
+import { fetchUserInfo, fetchSomeUserCards } from "../utils/apiQuery"; 
 
 function Main(props) {
 
     const userInfoQuery = useQuery("userInfo", fetchUserInfo, {staleTime: 50000})
-    const userCardsQuery = useQuery("userCards", fetchUserCards, {staleTime: 50000})
+    const userSomeCardsQuery = useInfiniteQuery(
+        "userSomeCards",
+        ({ pageParam = 1 }) => fetchSomeUserCards(pageParam),
+        {
+          getNextPageParam: (lastPage, allPages) => {
+            const maxPages = Math.ceil(lastPage.cardsLength / 9);
+            const nextPage = allPages.length + 1;
+            console.log(nextPage, maxPages);
+            return nextPage <= maxPages ? nextPage : undefined;
+            },
+        }
+      );
+    
+    
+      React.useEffect(() => {
+        let fetching = false;
+        const onScroll = async (event) => {
+          const { scrollHeight, scrollTop, clientHeight } =
+            event.target.scrollingElement;
+            
+          if (!fetching && scrollHeight - scrollTop <= clientHeight * 1.5) {
+            fetching = true;
+            await userSomeCardsQuery.fetchNextPage();
+            fetching = false;
+          }
+        };
+    
+        document.addEventListener("scroll", onScroll);
+        return () => {
+          document.removeEventListener("scroll", onScroll);
+        };
+      }, []);
 
     return (
         <SkeletonTheme  baseColor={"#2b2b2b"} highlightColor={"#474747"}>
@@ -30,14 +61,16 @@ function Main(props) {
                 <button className="profile__add-btn" type="button" aria-label="add new card" onClick={props.onAddPlace}></button>
             </section>
 
-            {userCardsQuery.data ? 
+            {userSomeCardsQuery.data ? 
             
                 <section className="elements">
-                    {userCardsQuery.data?.slice(0).reverse().map((item) => (
+                    {userSomeCardsQuery.data.pages?.map((page) =>
+                        page.cards.map((item) => (
                             <Card key={item._id} item={item} name={item.name} link={item.link} likes={item.likes} onCardClick={props.onCardClick} onCardLike={props.onCardLike} onCardDelete={props.onCardDelete} userCards={props.userCards}/>
-                        ))}
+                        )))}
                     </section>
-                : <Skeleton count={6} containerClassName="elements_skeleton" className='element_skeleton' borderRadius={"10px"}/>
+            : 
+                <Skeleton count={6} containerClassName="elements_skeleton" className='element_skeleton' borderRadius={"10px"}/>
             }
 
         </main>
